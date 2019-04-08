afde80294ebbf2f764959ce60d13f7999f4d018c
timberwolf <=1402193626@qq.com>
2019-04-08 21:30:26 +0800

    修改SystemState.java文件

--- src/main/java/com/mtq/util/SystemState.java
+++ src/main/java/com/mtq/util/SystemState.java
@@ -1,7 +1,7 @@
 package com.mtq.util;
 
 public class SystemState {
-	public static final String REQUEST_SUCCESS = "success";
+	public static final String REQUEST_SUCCESS = "SUCCESS";
 	public static final String REQUEST_ERROR = "error";
 	public static final String REQUEST_NO_DATA = "nodata";
 	public static final String SESSEION_TIME_OUT = "time_out";
